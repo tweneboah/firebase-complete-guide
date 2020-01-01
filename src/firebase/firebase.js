8460f425a4f7ce56104830336a6979d48188e075
Emmanuel Tweneboah <twentekghana@gmail.com>
2020-01-01 10:27:09 +0800

    Integrating firebase to react

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -20,6 +20,7 @@ const database = firebase.database()
 
 
 // //CREATING DATABASE
+//When creating a database you can pass the name of the database to ref, if it's not there it will create it by that name
 // database.ref()
 //   .set({
 //     //set helps to write to our database
@@ -96,7 +97,159 @@ const database = firebase.database()
 
 
 
-database.ref().on('value', (snapshot) => {
-  console.log(snapshot.val())
-})
+// database.ref().on('value', (snapshot) => {
+//   console.log(snapshot.val())
+// })
 
+// //This is use to stop subscribing from updating
+// database.ref().off()
+
+
+
+//ARRAY DATABASE IN FIREBASE
+
+//firebase don't support arrays
+//Because of that we we will use push method which randomly creats id for each of our data
+
+// database.ref('notes').push({
+//   title: 'Go Home 2',
+//   body: 'Studying 2'
+// })
+
+
+//FETCHING ARRAY DATA
+
+//When retrieving array of data from firebase it returns as an object so to convert to array with proper id we will use a method on snapshot call forEach()
+
+// database.ref('notes').once('value').then((snapshot) => {
+
+//   const notes = []
+//   snapshot.forEach((childSnapshot) => {
+//     //since firebase don't automatically provide keys/id we have to do that by ourselvs by creating an object and assign our on key and then spread the rest of the data
+//     notes.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val()
+//     })
+//   })
+//   console.log(notes)
+// })
+
+
+//USING ON
+
+// database.ref('notes').on('value', (snapshot) => {
+//   const notes2 = []
+
+//   snapshot.forEach((childSnapshot) => {
+//     notes2.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val()
+//     })
+//   })
+//   console.log(notes2)
+// })
+
+
+//EVENTS
+
+//1. child_remove(): this fires when a data is removed
+
+//This will render the id and the content of the deleted data
+// database.ref('notes').on('child_moved', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val())
+// });
+
+// database.ref('notes').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val())
+// });
+
+// database.ref('notes').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val())
+// });
+
+
+
+
+//FIREBASE WITH REDUX/REACT
+
+//Component shouldn't be aware of where the data is coming from
+
+
+
+export {
+  firebase,
+  database
+}
+
+
+
+
+import database from '../firebase/firebasePromise';
+
+//create a new database
+//============================
+
+
+// database.ref('notes').push({
+//   description: '', //form fields
+//   body: '',
+//   title: ''
+// }).then((dataToBeCreated) => {
+//  return {
+//    id: dataToBeCreated.key,
+//    ...dataToBeCreated
+//  }
+// }).catch(() => {
+
+// });
+
+
+//FETCHING DATA
+//=======================
+
+
+//Use componentDidMount
+
+
+// componentDidMount() {
+//   // database.ref('notes').on('value', (snapshot) => {
+// //   const notes2 = []
+
+// //   snapshot.forEach((childSnapshot) => {
+// //     notes2.push({
+// //       id: childSnapshot.key,
+// //       ...childSnapshot.val()
+// //     })
+// //   })
+// //   console.log(notes2)
+// // })
+// }
+
+//REMOVING DATA
+//========================
+
+//You need an id and a fuction where to get the id
+
+
+const remeveNote = (id) => {
+  return database.ref(`notes/${id}`).remove().then(() => {
+    console.log(`Data removed`)
+  }).catch((e) => {
+    console.log(`Error occured`)
+  })
+}
+
+
+
+//UPDATE
+//=======================
+
+//This requires the id and the object to update
+
+const updateNote = (id, dataTobeUpdated) => {
+  return database.ref(`notes/${id}`).update(dataTobeUpdated).then(() => {
+
+  }).catch(() => {
+
+  })
+}
